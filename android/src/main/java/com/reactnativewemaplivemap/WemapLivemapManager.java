543d7c31f7068634f4ea1207c5a252b2edf440ac
Thibaud <thibaud.michel@gmail.com>
2020-11-25 16:22:22 +0100

    [Android] Create Livemap programmatically

--- android/src/main/java/com/reactnativewemaplivemap/WemapLivemapManager.java
+++ android/src/main/java/com/reactnativewemaplivemap/WemapLivemapManager.java
@@ -1,8 +1,7 @@
 package com.reactnativewemaplivemap;
 
-import android.content.Context;
-import android.view.LayoutInflater;
 
+import com.getwemap.livemap.sdk.LivemapOptions;
 import com.getwemap.livemap.sdk.LivemapView;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
@@ -19,8 +18,11 @@ public class WemapLivemapManager extends SimpleViewManager<LivemapView> {
 
   @Override
   public LivemapView createViewInstance(ThemedReactContext context) {
-    LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    return (LivemapView) inflater.inflate(R.layout.wemap_livemap, null);
+
+    LivemapOptions options = new LivemapOptions();
+    options.emmid = 9432;
+
+    return new LivemapView(context, options);
   }
 
 }
