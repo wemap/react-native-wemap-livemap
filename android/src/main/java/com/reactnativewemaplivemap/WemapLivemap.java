e6b84cc99a76952b3416f25a1de761e67feee721
Thibault Capelli <capellithibault@gmail.com>
2021-02-09 02:21:51 +0100

    build: bind events, methods and new props for android

---
+++ android/src/main/java/com/reactnativewemaplivemap/WemapLivemap.java
@@ -0,0 +1,57 @@
+package com.reactnativewemaplivemap;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
+import com.getwemap.livemap.sdk.Livemap;
+import com.getwemap.livemap.sdk.LivemapView;
+import com.getwemap.livemap.sdk.OnLivemapReadyCallback;
+import com.getwemap.livemap.sdk.callbacks.PinpointOpenListener;
+
+import java.util.concurrent.Callable;
+
+public class WemapLivemap extends LivemapView implements OnLivemapReadyCallback {
+
+  public Livemap livemap;
+
+  public WemapLivemap(Context context) {
+    super(context);
+    this.getLivemapAsync(this);
+  }
+
+  private void sendNativeEvent(String name, BuildEvent buildEvent) {
+    WritableMap event = Arguments.createMap();
+
+    buildEvent.build(event);
+
+    ReactContext reactContext = (ReactContext)getContext();
+    reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(getId(), name + "Event", event);
+  }
+
+  @Override
+  public void onLivemapReady(Livemap livemap) {
+    Log.e("onLivemapReady", "map ready");
+    this.livemap = livemap;
+
+    sendNativeEvent("onMapReady", event -> event.putNull("value"));
+
+    livemap.addPinpointOpenListener((pinpoint) -> sendNativeEvent("onPinpointOpen", e -> e.putInt("id", pinpoint.getId())));
+    livemap.addPinpointCloseListener(() -> sendNativeEvent("onPinpointClose", e -> e = null));
+    livemap.addUserLoginListener(() -> sendNativeEvent("onUserLogin", e -> e = null));
+    livemap.addUserLogoutListener(() -> sendNativeEvent("onUserLogout", e -> e = null));
+    livemap.addEventOpenListener((event) -> sendNativeEvent("onEventOpen", e -> e.putInt("id", event.getId())));
+    livemap.addEventCloseListener(() -> sendNativeEvent("onEventClose", e -> e = null));
+    livemap.addGuidingStartedListener(() -> sendNativeEvent("onGuidingStarted", e -> e = null));
+    livemap.addGuidingStoppedListener(() -> sendNativeEvent("onGuidingStopped", e -> e = null));
+  }
+}
+
+interface BuildEvent {
+  void build(WritableMap event);
+}
--- android/src/main/java/com/reactnativewemaplivemap/WemapLivemapManager.java
+++ android/src/main/java/com/reactnativewemaplivemap/WemapLivemapManager.java
@@ -1,28 +1,116 @@
 package com.reactnativewemaplivemap;
 
 
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.common.MapBuilder;
 import com.getwemap.livemap.sdk.LivemapOptions;
-import com.getwemap.livemap.sdk.LivemapView;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
+import com.getwemap.livemap.sdk.model.Filters;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
-public class WemapLivemapManager extends SimpleViewManager<LivemapView> {
+import java.util.Map;
+
+public class WemapLivemapManager extends SimpleViewManager<WemapLivemap> {
 
   public static final String REACT_CLASS = "WemapLivemap";
+  private ThemedReactContext mContext;
 
   @Override
   public String getName() {
     return REACT_CLASS;
   }
 
+  @ReactProp(name = "mapConfig")
+  public void setMapConfig(WemapLivemap view, ReadableMap mapConfig) {
+
+    LivemapOptions livemapOptions = new LivemapOptions();
+
+    livemapOptions.token = mapConfig.getString("token");
+
+    if(mapConfig.hasKey("webappEndpoint")) {
+      livemapOptions.webappEndpoint = mapConfig.getString("webappEndpoint");
+    }
+
+    if(mapConfig.hasKey("ufe")) {
+      livemapOptions.ufe = mapConfig.getBoolean("ufe");
+    }
+
+    if(mapConfig.hasKey("emmid")) {
+      livemapOptions.emmid = mapConfig.getInt("emmid");
+    }
+
+    view.loadMap(this.mContext, livemapOptions);
+  }
+
+  @NonNull
   @Override
-  public LivemapView createViewInstance(ThemedReactContext context) {
+  protected WemapLivemap createViewInstance(@NonNull ThemedReactContext reactContext) {
+    this.mContext = reactContext;
+    return new WemapLivemap(reactContext);
+  }
 
-    LivemapOptions options = new LivemapOptions();
-    options.emmid = 9432;
+  @Nullable
+  @Override
+  public Map<String, Object> getExportedCustomBubblingEventTypeConstants() {
 
-    return new LivemapView(context, options);
+    MapBuilder.Builder events = MapBuilder.builder();
+
+    String[] eventNames = new String[]{
+      "onMapReady",
+      "onPinpointOpen",
+      "onPinpointClose",
+      "onUserLogin",
+      "onUserLogout",
+      "onEventOpen",
+      "onEventClose",
+      "onGuidingStarted",
+      "onGuidingStopped"
+    };
+
+    for (String eventName : eventNames) {
+      Map eventHandler = MapBuilder.of("phasedRegistrationNames", MapBuilder.of("bubbled", eventName));
+      events.put(eventName + "Event", eventHandler);
+    }
+
+    return events.build();
+  }
+
+  @Override
+  public void receiveCommand(@NonNull WemapLivemap root, String commandId, @Nullable ReadableArray args) {
+    super.receiveCommand(root, commandId, args);
+
+    switch (commandId) {
+      case "openEventViaManager":
+        root.livemap.openEvent(args.getInt(0));
+        break;
+      case "closeEventViaManager":
+        root.livemap.closeEvent();
+        break;
+      case "openPinpointViaManager":
+        root.livemap.openPinpoint(args.getInt(0));
+        break;
+      case "closePinpointViaManager":
+        root.livemap.closePinpoint();
+        break;
+      case "setFiltersViaManager":
+        Filters filters = new Filters(args.getString(0), args.getString(1), args.getString(2), (String[]) args.getArray(3).toArrayList().toArray());
+        root.livemap.setFilters(filters);
+        break;
+      case "navigateToPinpointViaManager":
+        root.livemap.navigateToPinpoint(args.getInt(0));
+        break;
+      case "stopNavigationViaManager":
+        root.livemap.stopNavigation();
+        break;
+      case "signInByTokenViaManager":
+        root.livemap.signInByToken(args.getString(0), args.getString(1));
+        break;
+    }
   }
 
 }
