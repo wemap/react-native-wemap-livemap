a6789c42791ae64a634d6311d3c4970a98654d43
Thibault Capelli <capellithibault@gmail.com>
2021-02-09 02:23:20 +0100

    build: make new events, method and props avaible from JS

--- src/index.js
+++ src/index.js
@@ -7,30 +7,61 @@ import {
 
 const WemapLivemap = requireNativeComponent('WemapLivemap', null);
 
-const Livemap = (props, ref) => {
+const Livemap = forwardRef((props, ref) => {
   const wemapLivemap = useRef();
+
   useImperativeHandle(ref, () => ({
+    openEvent,
+    closeEvent,
     openPinpoint,
+    closePinpoint,
+    setFilters,
+    navigateToPinpoint,
+    stopNavigation,
     signInByToken,
   }));
 
   const sendCommand = (command, params) => {
     UIManager.dispatchViewManagerCommand(
       findNodeHandle(wemapLivemap.current),
-      UIManager.WemapLivemap.Commands[command],
+      command,
       params
     );
+  };
+
+  const openEvent = (id) => {
+    sendCommand('openEventViaManager', [id]);
+  };
+
+  const closeEvent = () => {
+    sendCommand('closeEventViaManager');
   };
 
   const openPinpoint = (id) => {
     sendCommand('openPinpointViaManager', [id]);
   };
 
+  const closePinpoint = (id) => {
+    sendCommand('closePinpointViaManager');
+  };
+
+  const setFilters = (startDate, endDate, query, tags) => {
+    sendCommand('setFiltersViaManager', [startDate, endDate, query, tags]);
+  };
+
+  const navigateToPinpoint = (id) => {
+    sendCommand('navigateToPinpointViaManager', [id]);
+  };
+
+  const stopNavigation = () => {
+    sendCommand('stopNavigationViaManager');
+  };
+
   const signInByToken = (access_token, refresh_token) => {
     sendCommand('signInByTokenViaManager', [access_token, refresh_token]);
   };
 
-  return <WemapLivemap ref={wemapLivemap} style={{ flex: 1 }} {...props} />;
-};
+  return <WemapLivemap ref={wemapLivemap} {...props} />;
+});
 
-export default forwardRef(Livemap);
+export default Livemap;
