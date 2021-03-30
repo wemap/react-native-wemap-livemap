a05bfc3db6f5eca66a2a718bcb66d400d7634aad
Thibault Capelli <capellithibault@gmail.com>
2021-03-30 12:28:57 +0200

    chore: add output argument to docs script

--- utils/react-docgen-markdown-renderer/index.js
+++ utils/react-docgen-markdown-renderer/index.js
@@ -6,9 +6,17 @@ const template = require('./template');
 const sourcePath = path.join(__dirname, '../../src/index.js');
 
 fs.readFile(sourcePath, 'utf8', (error, content) => {
-  fs.mkdirSync(path.join(__dirname, '../../docs'), { recursive: true });
-  const outputPath = path.join(__dirname, '../../docs/Livemap.md');
-  const data = reactDocgen.parse(content, null, null, { filename: sourcePath });
+  const outputArgNameIndex = process.argv.indexOf('-o');
+  const outputArg = process.argv[outputArgNameIndex + 1];
 
-  fs.writeFile(outputPath, template(data), () => {});
+  if (outputArgNameIndex !== -1 && outputArg) {
+    const outputPath = path.resolve(outputArg);
+    fs.mkdirSync(outputPath, { recursive: true });
+    const contentOutputPath = path.join(outputPath, 'Livemap.md');
+    const data = reactDocgen.parse(content, null, null, { filename: sourcePath });
+
+    fs.writeFile(contentOutputPath, template(data), () => {});
+  } else {
+    console.error('Please, provide an output path');
+  }
 });
