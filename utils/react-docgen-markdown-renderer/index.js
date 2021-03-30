27e29d9c79b25773d81e0e285ad46aa10d400df0
Thibault Capelli <capellithibault@gmail.com>
2021-03-30 13:46:54 +0200

    fix: output argument for npm in docs script

--- utils/react-docgen-markdown-renderer/index.js
+++ utils/react-docgen-markdown-renderer/index.js
@@ -6,14 +6,15 @@ const template = require('./template');
 const sourcePath = path.join(__dirname, '../../src/index.js');
 
 fs.readFile(sourcePath, 'utf8', (error, content) => {
-  const outputArgNameIndex = process.argv.indexOf('-o');
-  const outputArg = process.argv[outputArgNameIndex + 1];
+  const argv = JSON.parse(process.env.npm_config_argv).original;
+  const outputArgNameIndex = argv.indexOf('-o');
+  const outputArg = argv[outputArgNameIndex + 1];
 
   if (outputArgNameIndex !== -1 && outputArg) {
     const outputPath = path.resolve(outputArg);
     fs.mkdirSync(outputPath, { recursive: true });
-    const contentOutputPath = path.join(outputPath, 'Livemap.md');
     const data = reactDocgen.parse(content, null, null, { filename: sourcePath });
+    const contentOutputPath = path.join(outputPath, `${data.displayName}.md`);
 
     fs.writeFile(contentOutputPath, template(data), () => {});
   } else {
